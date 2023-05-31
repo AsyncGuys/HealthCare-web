1daf9b275e0c78a9290b1256e7d1d3f73c59cbb4
yashc88 <yashchoudhary88505@gmail.com>
2023-05-31 23:44:36 +0530

    bot removed

--- frontend/src/Pages/bot.jsx
+++ frontend/src/Pages/bot.jsx
@@ -1,80 +1,9 @@
-import { useState, useEffect, useRef } from 'react';
-import axios from 'axios';
+import React from 'react'
 
-
-const Bot = () => {
-  const [messages, setMessages] = useState([]);
-  const [inputMessage, setInputMessage] = useState('');
-  const messagesEndRef = useRef(null);
-
-  useEffect(() => {
-    // Fetch initial message from the backend server
-    const fetchInitialMessage = async () => {
-      try {
-        const response = await axios.post('/api/chat', { message: 'Hello' }); // Replace with your API endpoint for chat
-        setMessages([{ text: response.data, isUser: false }]);
-      } catch (error) {
-        console.error('Error fetching initial message:', error);
-      }
-    };
-
-    fetchInitialMessage();
-  }, []);
-
-  useEffect(() => {
-    // Scroll to the bottom of the messages
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  }, [messages]);
-
-  const handleSendMessage = async () => {
-    if (!inputMessage) return;
-
-    setMessages((prevMessages) => [...prevMessages, { text: inputMessage, isUser: true }]);
-    setInputMessage('');
-
-    try {
-      // Send user message to the backend server
-      const response = await axios.post('/api/chat', { message: inputMessage }); // Replace with your API endpoint for chat
-      setMessages((prevMessages) => [...prevMessages, { text: response.data, isUser: false }]);
-    } catch (error) {
-      console.error('Error sending message:', error);
-    }
-  };
-
+const bot = () => {
   return (
-    <div className="flex flex-col h-screen">
-      <div className="flex-1 overflow-y-auto px-4 py-8">
-        {messages.map((message, index) => (
-          <div
-            key={index}
-            className={`mb-4 p-4 rounded-lg max-w-md ${
-              message.isUser ? 'bg-blue-500 text-white self-end' : 'bg-gray-200'
-            }`}
-          >
-            <p>{message.text}</p>
-          </div>
-        ))}
-        <div ref={messagesEndRef} />
-      </div>
-      <div className="p-4 bg-[#E5E0FF]">
-        <div className="flex">
-          <input
-            type="text"
-            value={inputMessage}
-            onChange={(e) => setInputMessage(e.target.value)}
-            placeholder="Type your query...."
-            className="flex-grow px-3 py-2 rounded-l-md border border-[#ffffff] focus:outline-none"
-          />
-          <button
-            onClick={handleSendMessage}
-            className="px-4 py-2 bg-[#674188] text-white rounded-r-lg hover:bg-[#5D3891] focus:outline-none"
-          >
-            Send
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
+    <div>bot</div>
+  )
+}
 
-export default Bot;
+export default bot
--- frontend/src/Pages/map.jsx
+++ frontend/src/Pages/map.jsx
@@ -3,31 +3,29 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet/dist/images/marker-icon.png';
 import 'leaflet/dist/images/marker-shadow.png';
-import hospitalsData from '../components/Main/hospitalsData'; // Assuming you have the hospital data in a separate file
+import hospitalsData from '../components/Main/hospitalsData'; 
 
 const Map = () => {
   const mapContainerRef = useRef(null);
-  const latitude = 51.5074; // Latitude of the center point
-  const longitude = -0.1278; // Longitude of the center point
+  const latitude = 28.6741; 
+  const longitude =   77.1339;  
   const zoomLevel = 10; // Zoom level for the map
 
   useEffect(() => {
-    // Initialize Leaflet map
+
     const map = L.map(mapContainerRef.current).setView([latitude, longitude], zoomLevel);
 
-    // Add the tile layer for the map (you can change the tile URL to your preference)
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: 'Map data &copy; OpenStreetMap contributors',
     }).addTo(map);
 
-    // Loop through the hospitals data and add markers to the map
+
     hospitalsData.forEach((hospital) => {
       L.marker([hospital.latitude, hospital.longitude])
         .addTo(map)
         .bindPopup(hospital.name);
     });
 
-    // Clean up when component is unmounted
     return () => {
       map.remove();
     };
